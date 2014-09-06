589746e37163272c0070627cc5e5718a725873a4
Andrey Malets <andrey.malets@gmail.com>
2014-09-07 00:07:08 +0600

    Added querycmd tool subcommand.
    Added a tool to query commands required to build all outputs
    of given target(s). This may be convenient as a quick check that
    the source file compiles prolerly during the editing/compiling
    cycle, for example.

--- src/ninja.cc
+++ src/ninja.cc
@@ -104,6 +104,7 @@ struct NinjaMain : public BuildLogUser {
   // The various subcommands, run via "-t XXX".
   int ToolGraph(int argc, char* argv[]);
   int ToolQuery(int argc, char* argv[]);
+  int ToolQueryCmd(int argc, char* argv[]);
   int ToolDeps(int argc, char* argv[]);
   int ToolBrowse(int argc, char* argv[]);
   int ToolMSVC(int argc, char* argv[]);
@@ -359,6 +360,27 @@ int NinjaMain::ToolQuery(int argc, char* argv[]) {
         printf("    %s\n", (*out)->path().c_str());
       }
     }
+  }
+  return 0;
+}
+
+int NinjaMain::ToolQueryCmd(int argc, char* argv[]) {
+  if (argc == 0) {
+    Error("expected a target to query");
+    return 1;
+  }
+
+  for (int i = 0; i < argc; ++i) {
+    string err;
+    Node* node = CollectTarget(argv[i], &err);
+    if (!node) {
+      Error("%s", err.c_str());
+      return 1;
+    }
+
+    for (vector<Edge*>::const_iterator edge = node->out_edges().begin();
+         edge != node->out_edges().end(); ++edge)
+      puts((*edge)->EvaluateCommand().c_str());
   }
   return 0;
 }
@@ -714,6 +736,8 @@ const Tool* ChooseTool(const string& tool_name) {
       Tool::RUN_AFTER_LOAD, &NinjaMain::ToolGraph },
     { "query", "show inputs/outputs for a path",
       Tool::RUN_AFTER_LOGS, &NinjaMain::ToolQuery },
+    { "querycmd", "list commands to build all outputs of given targets",
+      Tool::RUN_AFTER_LOGS, &NinjaMain::ToolQueryCmd },
     { "targets",  "list targets by their rule or depth in the DAG",
       Tool::RUN_AFTER_LOAD, &NinjaMain::ToolTargets },
     { "compdb",  "dump JSON compilation database to stdout",
